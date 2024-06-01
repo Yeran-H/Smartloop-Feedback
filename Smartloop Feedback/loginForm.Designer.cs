678373da4d8f2c5324646a67c81ce95da07c0ad6
Yeran-H <13888767@student.uts.edu.au>
2024-06-01 13:00:02 +1000

    Desigin UI for login page. No image

---
+++ Smartloop Feedback/loginForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Smartloop_Feedback
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.usernameTb = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.passwordTb = new System.Windows.Forms.TextBox();
+            this.signBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.exitPb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-51, -80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 313);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // usernameTb
+            // 
+            this.usernameTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(22)))), ((int)(((byte)(39)))));
+            this.usernameTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.usernameTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.usernameTb.HideSelection = false;
+            this.usernameTb.Location = new System.Drawing.Point(66, 183);
+            this.usernameTb.Name = "usernameTb";
+            this.usernameTb.Size = new System.Drawing.Size(222, 20);
+            this.usernameTb.TabIndex = 2;
+            this.usernameTb.TabStop = false;
+            this.usernameTb.Text = "Username";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(39, 207);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(39, 262);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 1);
+            this.panel2.TabIndex = 6;
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(22)))), ((int)(((byte)(39)))));
+            this.passwordTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.passwordTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.passwordTb.HideSelection = false;
+            this.passwordTb.Location = new System.Drawing.Point(67, 238);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(222, 20);
+            this.passwordTb.TabIndex = 5;
+            this.passwordTb.TabStop = false;
+            this.passwordTb.Text = "Password";
+            // 
+            // signBtn
+            // 
+            this.signBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
+            this.signBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signBtn.Font = new System.Drawing.Font("Aptos Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(22)))), ((int)(((byte)(39)))));
+            this.signBtn.Location = new System.Drawing.Point(37, 294);
+            this.signBtn.Name = "signBtn";
+            this.signBtn.Size = new System.Drawing.Size(252, 52);
+            this.signBtn.TabIndex = 7;
+            this.signBtn.Text = "Sigin In";
+            this.signBtn.UseVisualStyleBackColor = false;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(22)))), ((int)(((byte)(39)))));
+            this.registerBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerBtn.Font = new System.Drawing.Font("Aptos Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
+            this.registerBtn.Location = new System.Drawing.Point(37, 352);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(252, 52);
+            this.registerBtn.TabIndex = 8;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            // 
+            // exitPb
+            // 
+            this.exitPb.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitPb.Image = ((System.Drawing.Image)(resources.GetObject("exitPb.Image")));
+            this.exitPb.Location = new System.Drawing.Point(326, 0);
+            this.exitPb.Name = "exitPb";
+            this.exitPb.Size = new System.Drawing.Size(21, 21);
+            this.exitPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.exitPb.TabIndex = 9;
+            this.exitPb.TabStop = false;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(22)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(346, 446);
+            this.Controls.Add(this.exitPb);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.signBtn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.passwordTb);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.usernameTb);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "loginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitPb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox usernameTb;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox passwordTb;
+        private System.Windows.Forms.Button signBtn;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.PictureBox exitPb;
+    }
+}
+
