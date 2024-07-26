e0544b315c75734e3e24fb3fc04a3c6cc3c59404
Yeran-H <13888767@student.uts.edu.au>
2024-07-26 12:52:25 +1000

    Course UI

--- Smartloop Feedback/Settings/EditCourseForm.Designer.cs
+++ Smartloop Feedback/Settings/EditCourseForm.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.canvasPl = new System.Windows.Forms.Panel();
+            this.canvasTb = new System.Windows.Forms.TextBox();
+            this.descriptionPl = new System.Windows.Forms.Panel();
+            this.descriptionTb = new System.Windows.Forms.TextBox();
+            this.creditPl = new System.Windows.Forms.Panel();
+            this.creditTb = new System.Windows.Forms.TextBox();
+            this.namePl = new System.Windows.Forms.Panel();
+            this.nameTb = new System.Windows.Forms.TextBox();
+            this.codePl = new System.Windows.Forms.Panel();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.assessmentBtn = new System.Windows.Forms.Button();
+            this.codeTb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(0, 0);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(734, 424);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.canvasPl);
+            this.tabPage1.Controls.Add(this.canvasTb);
+            this.tabPage1.Controls.Add(this.descriptionPl);
+            this.tabPage1.Controls.Add(this.descriptionTb);
+            this.tabPage1.Controls.Add(this.creditPl);
+            this.tabPage1.Controls.Add(this.creditTb);
+            this.tabPage1.Controls.Add(this.namePl);
+            this.tabPage1.Controls.Add(this.nameTb);
+            this.tabPage1.Controls.Add(this.codePl);
+            this.tabPage1.Controls.Add(this.cancelBtn);
+            this.tabPage1.Controls.Add(this.assessmentBtn);
+            this.tabPage1.Controls.Add(this.codeTb);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(726, 398);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // canvasPl
+            // 
+            this.canvasPl.BackColor = System.Drawing.Color.White;
+            this.canvasPl.Location = new System.Drawing.Point(262, 237);
+            this.canvasPl.Name = "canvasPl";
+            this.canvasPl.Size = new System.Drawing.Size(206, 1);
+            this.canvasPl.TabIndex = 32;
+            // 
+            // canvasTb
+            // 
+            this.canvasTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.canvasTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.canvasTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.canvasTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.canvasTb.HideSelection = false;
+            this.canvasTb.Location = new System.Drawing.Point(262, 215);
+            this.canvasTb.Name = "canvasTb";
+            this.canvasTb.Size = new System.Drawing.Size(206, 20);
+            this.canvasTb.TabIndex = 28;
+            this.canvasTb.Text = "Please Enter Canvas Link";
+            // 
+            // descriptionPl
+            // 
+            this.descriptionPl.BackColor = System.Drawing.Color.White;
+            this.descriptionPl.Location = new System.Drawing.Point(263, 211);
+            this.descriptionPl.Name = "descriptionPl";
+            this.descriptionPl.Size = new System.Drawing.Size(206, 1);
+            this.descriptionPl.TabIndex = 35;
+            // 
+            // descriptionTb
+            // 
+            this.descriptionTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.descriptionTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.descriptionTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.descriptionTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.descriptionTb.HideSelection = false;
+            this.descriptionTb.Location = new System.Drawing.Point(259, 120);
+            this.descriptionTb.Multiline = true;
+            this.descriptionTb.Name = "descriptionTb";
+            this.descriptionTb.Size = new System.Drawing.Size(206, 85);
+            this.descriptionTb.TabIndex = 27;
+            this.descriptionTb.Text = "Description";
+            // 
+            // creditPl
+            // 
+            this.creditPl.BackColor = System.Drawing.Color.White;
+            this.creditPl.Location = new System.Drawing.Point(261, 113);
+            this.creditPl.Name = "creditPl";
+            this.creditPl.Size = new System.Drawing.Size(206, 1);
+            this.creditPl.TabIndex = 34;
+            // 
+            // creditTb
+            // 
+            this.creditTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.creditTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.creditTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.creditTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.creditTb.HideSelection = false;
+            this.creditTb.Location = new System.Drawing.Point(261, 94);
+            this.creditTb.Name = "creditTb";
+            this.creditTb.Size = new System.Drawing.Size(206, 20);
+            this.creditTb.TabIndex = 26;
+            this.creditTb.Text = "Credit Point";
+            // 
+            // namePl
+            // 
+            this.namePl.BackColor = System.Drawing.Color.White;
+            this.namePl.Location = new System.Drawing.Point(262, 86);
+            this.namePl.Name = "namePl";
+            this.namePl.Size = new System.Drawing.Size(206, 1);
+            this.namePl.TabIndex = 33;
+            // 
+            // nameTb
+            // 
+            this.nameTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.nameTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nameTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.nameTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.nameTb.HideSelection = false;
+            this.nameTb.Location = new System.Drawing.Point(262, 64);
+            this.nameTb.Name = "nameTb";
+            this.nameTb.Size = new System.Drawing.Size(206, 20);
+            this.nameTb.TabIndex = 25;
+            this.nameTb.Text = "Course Name";
+            // 
+            // codePl
+            // 
+            this.codePl.BackColor = System.Drawing.Color.White;
+            this.codePl.Location = new System.Drawing.Point(262, 59);
+            this.codePl.Name = "codePl";
+            this.codePl.Size = new System.Drawing.Size(206, 1);
+            this.codePl.TabIndex = 31;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.cancelBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelBtn.Font = new System.Drawing.Font("Aptos Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
+            this.cancelBtn.Location = new System.Drawing.Point(487, 293);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(211, 55);
+            this.cancelBtn.TabIndex = 30;
+            this.cancelBtn.Text = "Delete Course";
+            this.cancelBtn.UseVisualStyleBackColor = false;
+            // 
+            // assessmentBtn
+            // 
+            this.assessmentBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
+            this.assessmentBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.assessmentBtn.Enabled = false;
+            this.assessmentBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.assessmentBtn.Font = new System.Drawing.Font("Aptos Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.assessmentBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.assessmentBtn.Location = new System.Drawing.Point(53, 293);
+            this.assessmentBtn.Name = "assessmentBtn";
+            this.assessmentBtn.Size = new System.Drawing.Size(211, 55);
+            this.assessmentBtn.TabIndex = 29;
+            this.assessmentBtn.Text = "View Assessment";
+            this.assessmentBtn.UseVisualStyleBackColor = false;
+            // 
+            // codeTb
+            // 
+            this.codeTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.codeTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.codeTb.Font = new System.Drawing.Font("Aptos", 12F);
+            this.codeTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(193)))), ((int)(((byte)(193)))));
+            this.codeTb.HideSelection = false;
+            this.codeTb.Location = new System.Drawing.Point(262, 37);
+            this.codeTb.Name = "codeTb";
+            this.codeTb.Size = new System.Drawing.Size(206, 20);
+            this.codeTb.TabIndex = 24;
+            this.codeTb.Text = "Course Code";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(34)))), ((int)(((byte)(61)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Aptos Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
+            this.button1.Location = new System.Drawing.Point(270, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 55);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Update Course";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // EditCourseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(734, 424);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EditCourseForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EditCourse";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel canvasPl;
+        private System.Windows.Forms.TextBox canvasTb;
+        private System.Windows.Forms.Panel descriptionPl;
+        private System.Windows.Forms.TextBox descriptionTb;
+        private System.Windows.Forms.Panel creditPl;
+        private System.Windows.Forms.TextBox creditTb;
+        private System.Windows.Forms.Panel namePl;
+        private System.Windows.Forms.TextBox nameTb;
+        private System.Windows.Forms.Panel codePl;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button assessmentBtn;
+        private System.Windows.Forms.TextBox codeTb;
     }
 }
