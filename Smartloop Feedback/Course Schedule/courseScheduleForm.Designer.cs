8f1ce22c7e50d34efae6d5e0b8417df6fc891684
Yeran-H <13888767@student.uts.edu.au>
2024-08-02 11:03:23 +1000

    add hourly view

--- Smartloop Feedback/Course Schedule/courseScheduleForm.Designer.cs
+++ Smartloop Feedback/Course Schedule/courseScheduleForm.Designer.cs
@@ -44,23 +44,23 @@
             // 
             this.calendarTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 730F));
             this.calendarTable.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.calendarTable.Location = new System.Drawing.Point(0, 50);
+            this.calendarTable.Location = new System.Drawing.Point(0, 56);
             this.calendarTable.Name = "calendarTable";
             this.calendarTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.calendarTable.Size = new System.Drawing.Size(730, 388);
+            this.calendarTable.Size = new System.Drawing.Size(730, 382);
             this.calendarTable.TabIndex = 6;
             // 
             // headerPanel
             // 
+            this.headerPanel.Controls.Add(this.toggleViewBtn);
             this.headerPanel.Controls.Add(this.addBtn);
             this.headerPanel.Controls.Add(this.previousPb);
             this.headerPanel.Controls.Add(this.nextPb);
             this.headerPanel.Controls.Add(this.monthLb);
-            this.headerPanel.Controls.Add(this.toggleViewBtn);
             this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.headerPanel.Location = new System.Drawing.Point(0, 0);
             this.headerPanel.Name = "headerPanel";
-            this.headerPanel.Size = new System.Drawing.Size(730, 50);
+            this.headerPanel.Size = new System.Drawing.Size(730, 56);
             this.headerPanel.TabIndex = 7;
             // 
             // addBtn
@@ -72,7 +72,7 @@
             this.addBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
             this.addBtn.Location = new System.Drawing.Point(578, 4);
             this.addBtn.Name = "addBtn";
-            this.addBtn.Size = new System.Drawing.Size(140, 43);
+            this.addBtn.Size = new System.Drawing.Size(140, 49);
             this.addBtn.TabIndex = 9;
             this.addBtn.TabStop = false;
             this.addBtn.Text = "Add Event";
@@ -119,12 +119,12 @@
             this.toggleViewBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.toggleViewBtn.Font = new System.Drawing.Font("Aptos Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toggleViewBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(0)))), ((int)(((byte)(57)))));
-            this.toggleViewBtn.Location = new System.Drawing.Point(427, 4);
+            this.toggleViewBtn.Location = new System.Drawing.Point(421, 4);
             this.toggleViewBtn.Name = "toggleViewBtn";
-            this.toggleViewBtn.Size = new System.Drawing.Size(140, 43);
+            this.toggleViewBtn.Size = new System.Drawing.Size(146, 52);
             this.toggleViewBtn.TabIndex = 10;
             this.toggleViewBtn.TabStop = false;
-            this.toggleViewBtn.Text = "Toggle View";
+            this.toggleViewBtn.Text = "Toggle to Monthly View";
             this.toggleViewBtn.UseVisualStyleBackColor = false;
             this.toggleViewBtn.Click += new System.EventHandler(this.ToggleViewButton_Click);
             // 
